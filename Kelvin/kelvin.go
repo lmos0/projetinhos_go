8b9117af1fe8bc7d267e59ea0e823132449b10ff
lmos0 <125509344+lmos0@users.noreply.github.com>
2023-06-04 13:02:05 -0300

    copia 1

---
+++ Kelvin/kelvin.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const kelvin_agua = 373.15
+	var c_agua = kelvin_agua - 273.15
+	fmt.Println("O ponto de ebulição da água em Kelvin é ", kelvin_agua, "E em Celsius é ", c_agua)
+
+}
